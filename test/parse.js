b673d2759eee137a863e14b07a343dc387a6d312
Ahmad Nassri <ahmad@ahmadnassri.com>
2020-11-01 16:28:40 -0500

    test(windows): normalize path names for windows tests

--- test/parse.js
+++ test/parse.js
@@ -1,41 +1,35 @@
 const { test } = require('tap')
-
+const { normalize } = require('path')
 const { parse } = require('../lib/parse')
-
-test('cannot parse file path as url', async assert => {
-  assert.plan(1)
-
-  assert.rejects(() => parse('not-a-real-path'))
-})
 
 test('cannot find path on fs', async assert => {
   assert.plan(1)
 
-  assert.match(await parse('/not-a-file'), { path: '/not-a-file' })
+  assert.match(await parse('/not-a-file'), { path: normalize('/not-a-file') })
 })
 
 test('append index on /', async assert => {
   assert.plan(1)
 
-  assert.match(await parse('/not-a-path/'), { path: '/not-a-path/index.html' })
+  assert.match(await parse('/not-a-path/'), { path: normalize('/not-a-path/index.html') })
 })
 
 test('append custom index on /', async assert => {
   assert.plan(1)
 
-  assert.match(await parse('/not-a-path/', { index: 'foo.bar' }), { path: '/not-a-path/foo.bar' })
+  assert.match(await parse('/not-a-path/', { index: 'foo.bar' }), { path: normalize('/not-a-path/foo.bar') })
 })
 
 test('defined root', async assert => {
   assert.plan(1)
 
-  assert.match(await parse('/not-a-path', { root: '/foo/bar' }), { path: '/foo/bar/not-a-path' })
+  assert.match(await parse('/not-a-path', { root: '/foo/bar' }), { path: normalize('/foo/bar/not-a-path') })
 })
 
 test('real path', async assert => {
   assert.plan(1)
 
-  assert.match(await parse('/test/fixtures/index.html'), { path: '/test/fixtures/index.html' })
+  assert.match(await parse('/test/fixtures/index.html'), { path: normalize('/test/fixtures/index.html') })
 })
 
 test('unknown mimetyle', async assert => {
@@ -53,5 +47,5 @@ test('read content', async assert => {
 test('traverse directory', async assert => {
   assert.plan(1)
 
-  assert.match(await parse('/test/fixtures/page.html'), { path: '/test/fixtures/page.html/index.html' })
+  assert.match(await parse('/test/fixtures/page.html'), { path: normalize('/test/fixtures/page.html/index.html') })
 })
